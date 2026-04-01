853e54e2a983e099ec19c2fb6bebd4336bbfa60e
Shafiun Miraz(মিরাজ) <shafiunmiraz@gmail.com>
2026-04-01 19:45:30 +0600

    fix: update entrypoint script to use a writable runtime config for coturn

--- turn-server/entrypoint.sh
+++ turn-server/entrypoint.sh
@@ -5,11 +5,15 @@ TURN_USER="${TURN_USER:-bromedia}"
 TURN_PASSWORD="${TURN_PASSWORD:-bromedia-secret}"
 TURN_REALM="${TURN_REALM:-bromedia.local}"
 
-CONF_FILE="/etc/coturn/turnserver.conf"
+TEMPLATE_FILE="/etc/coturn/turnserver.conf"
+RUNTIME_CONF="/tmp/turnserver.conf"
 
-# Patch the config with runtime environment variables
-sed -i "s/^user=.*/user=${TURN_USER}:${TURN_PASSWORD}/" "$CONF_FILE"
-sed -i "s/^realm=.*/realm=${TURN_REALM}/" "$CONF_FILE"
+# Create a writable runtime config from the read-only template.
+cp "$TEMPLATE_FILE" "$RUNTIME_CONF"
+
+# Patch config with runtime environment variables
+sed -i "s/^user=.*/user=${TURN_USER}:${TURN_PASSWORD}/" "$RUNTIME_CONF"
+sed -i "s/^realm=.*/realm=${TURN_REALM}/" "$RUNTIME_CONF"
 
 echo "[entrypoint] Starting coturn with user=${TURN_USER}, realm=${TURN_REALM}"
-exec turnserver -c "$CONF_FILE"
+exec turnserver -c "$RUNTIME_CONF"
